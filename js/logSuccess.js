577b93003da9dc563d3cf41daf4b18502c8ca78a
MaoMeng72 <1150354806@qq.com>
2019-06-01 19:43:38 +0800

    登陆注册完成，购物车可直接拿localsto中的onoff，首页待完成项拿商品数据渲染页面

--- js/logSuccess.js
+++ js/logSuccess.js
@@ -2,12 +2,48 @@
     "use strict";
 
     class Logsuce{
-        constructor(){
+        constructor(options){
+            this.logout = options.logout;
+            this.back = options.back;
 
+            this.getData();
+        }
+        // 拿登陆的数据
+        getData(){
+            // console.log(localStorage.getItem("data"));
+            this.data = JSON.parse(localStorage.getItem("data"));
+            for(let i=0;i<this.data.length;i++){
+                if(this.data[i].onoff == 1){
+                    this.logout.innerHTML = `<span>欢迎您：</span>
+                                            <a href="#">${this.data[i].user}</a>
+                                            <a href="#" class="back">[退出]</a>
+                                            <a href="vip.html">[机构会员]</a>`;
+                    
+                }
+            }
+            this.addEvent();
+        }
+        addEvent(){
+            this.logout.addEventListener("click",(eve)=>{
+                let e = eve || window.event;
+                let target = e.target || e.srcElement;
+                if(target.className == "back"){
+                    for(let i=0;i<this.data.length;i++){
+                        this.data[i].onoff = 0;
+                        console.log(this.data)
+                    }
+                    this.logout.innerHTML = `<span>您好，请</span>
+                                            <a href="login/login.html">[登陆]</a>
+                                            <a href="login/register.html">[注册]</a>
+                                            <a href="vip.html">[机构会员]</a>`;
+                    localStorage.setItem("data",JSON.stringify(this.data));
+                }
+            })
         }
     }
 
     new Logsuce({
-        
+        logout:document.querySelector("#top .top-l"),
+        back:document.querySelectorAll("#top .top-l a")[1]
     })
 })();
